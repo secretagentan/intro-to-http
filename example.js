2783dfd0c18ab27d0edf882b2bd76dca62ac55b7
an nguyen <hello.an.ng@gmail.com>
2017-01-05 12:52:09 -0800

    saves new status codes

--- example.js
+++ example.js
@@ -1,18 +1,32 @@
 var http = require('http');
+var fs = require('fs');
 
 var hostname = '127.0.0.1';
 var port = 3000;
+
+// var data = fs.readFile('./index.html', 'utf-8', function(err, data){
+//   if (err) throw err;
+//   console.log(data);
+//   return data;
+// });
 
 var server = http.createServer(function(request, response) {
   console.log('Incoming ' + request.method + 'request!' + request.url);
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/html');
-  if ( request.url === '/goodbye') {
-    response.write('<h1>Goodbye!</h1>');
-  } else {
-    response.write('<h1>hello</h1>');
+
+  if ( request.url === '/' ) {
+    fs.readFile('./index.html', 'utf-8', function(err, data){
+      if (err) throw err;
+      response.statusCode = 200;
+      response.write(data);
+      response.end();
+    }) else {
+    response.statusCode = 404;
+    response.write("Not Found!");
+    response.end();
+    }
   }
-  response.end();
 });
 
 server.listen(port, hostname, function(){
